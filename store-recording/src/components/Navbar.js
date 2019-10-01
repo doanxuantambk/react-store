b01344af5012abfc79f0303c46763dc8bc175e2e
doanxuantambk <doanxuantam@gmail.com>
2019-10-02 06:47:46 +0700

    UPdate

---
+++ store-recording/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Narbar extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Hello from Narbar</h3>
+            </div>
+        )
+    }
+}
