808f52172ed87634532be3da3a619646a04688e8
Tam Doan <doanxuantam@gmail.com>
2019-10-02 18:06:11 +0700

    Update

--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -1,10 +1,29 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import logo from "../logo.svg";
+
 
 export default class Narbar extends Component{
     render(){
         return(
-            <div>
-                <h3>Hello from Narbar</h3>
+            <div className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src = {logo} alt = "store" className="navbar-brand"/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                        products
+                        </Link>
+                    </li>
+                </ul>
+
+                <Link to='/cart' className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus"></i>
+                        My Cart
+                    </button>
+                </Link>
             </div>
         )
     }
